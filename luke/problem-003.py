08e3796105351edbd981a4fe7a759bea2117f58d
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-12 15:58:37 +0000

    Problem 3 (correct)
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@6 735d8fd3-3d48-0410-ad09-b9318371deed

---
+++ luke/problem-003.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# http://projecteuler.net/index.php?section=problems&id=3
+import math
+
+def factors(x):
+	factors = []
+	
+	i = 1
+	while True:
+		if not x % i:
+			if not factors.count(i):
+				factors.append(i)
+				if not factors.count(x / i): # don't add perfect squares twice
+					factors.append(x / i)
+				factors.sort()
+			else:
+				break
+		
+		i += 1
+			
+	return factors
+	
+def isPrime(x):
+	i = 2
+	while i <= math.ceil(x / 2):
+		if not x % i:
+			return False
+			
+		i += 1
+	
+	return True
+
+answer = 0
+for i in factors(600851475143):
+	if isPrime(i):
+		answer = i
+		
+print answer
