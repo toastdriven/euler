eaf907298023362c97efbf5e8e272cae6452f705
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-12 18:25:46 +0000

    actually adding the file this time
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@15 735d8fd3-3d48-0410-ad09-b9318371deed

---
+++ luke/problem-006.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# http://projecteuler.net/index.php?section=problems&id=5
+import math
+
+sum_of_squares = 0
+i = 1
+while i <= 100:
+	sum_of_squares += int(math.pow(i, 2))
+	i += 1
+	
+sum_only = 0
+i = 1
+while i <= 100:
+	sum_only += i
+	i += 1
+		
+answer = int(math.pow(sum_only, 2) - sum_of_squares)
+print answer
