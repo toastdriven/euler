ad054f66b8143e51112144deb83c8057add88445
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-12 14:26:33 +0000

    First two Euler problems
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@3 735d8fd3-3d48-0410-ad09-b9318371deed

--- luke/problem-001.py
+++ luke/problem-001.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# http://projecteuler.net/index.php?section=problems&id=1
 answer = 0
 for i in range(1,1000):
 	if i % 3 or i % 5:
---
+++ luke/problem-002.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# http://projecteuler.net/index.php?section=problems&id=2
+precursor = 1
+cursor = 1
+answer = 0
+while cursor < 4000000:
+	# move the precursor and the cursor to the next
+	# number in the fibonacci sequence
+	t = precursor + cursor
+	precursor = cursor
+	cursor = t
+	
+	# if the cursor is a number that is even, add it
+	# to the final answer
+	if cursor % 2:
+		answer += cursor
+		
+print answer
