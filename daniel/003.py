3174ed87e99b6a7b0a8546125b4f61e75882699c
daniel <daniel@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-12 16:08:27 +0000

    Problem 3 complete and correct.
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@8 735d8fd3-3d48-0410-ad09-b9318371deed

--- daniel/003.py
+++ daniel/003.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python
+import math
+
+def is_prime(number):
+    for i in range(2, int(math.ceil(math.sqrt(number)))):
+        if number % i == 0.0:
+            return False
+    
+    return True
+
+# source_number = 13195
+source_number = 600851475143
+largest_prime = 0
+current = int(math.ceil(math.sqrt(source_number)))
+end_at = 2
+
+while current > end_at:
+    if source_number % current == 0.0:
+        if is_prime(current):
+            largest_prime = current
+            break
+    current -= 1
+
+print "The largest prime is: %s" % largest_prime
