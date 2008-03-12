ab3bb766e39f289c10e275032c3155d2f140e9d1
jason <jason@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-12 19:49:06 +0000

    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@19 735d8fd3-3d48-0410-ad09-b9318371deed

--- jason/problem-003.py
+++ jason/problem-003.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
-primes = []
+import math
 
-for i in range(1000) :
-    
+target = 600851475143
+
+def isPrime(num):
+    halfway = int(math.ceil(math.sqrt(num))) + 1
+    for i in range(2, halfway):
+        if not num % i:
+            return False
+    return True
+
+
+halfway = int(math.ceil(math.sqrt(target))) + 1
+divisors = [i for i in range(1, halfway) if not target % i]
+primes = [divisor for divisor in divisors if isPrime(divisor)]
+print max(primes)
