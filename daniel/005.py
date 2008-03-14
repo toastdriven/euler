ec85ddb9cf1e0c6b7867a1b7d464ff49420fc623
daniel <daniel@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-14 18:37:13 +0000

    Completed Problem 13.
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@37 735d8fd3-3d48-0410-ad09-b9318371deed

--- daniel/005.py
+++ daniel/005.py
@@ -1,13 +1,2 @@
 #!/usr/bin/env python
-start_at = 1
-end_at = 10
-
-def greatest_common_number(start_at, end_at):
-    gcn = 1
-    
-    for i in xrange(start_at, end_at):
-        gcn *= i
-    
-    return gcn
-
-print greatest_common_number(start_at, end_at)
+print "I'm too stupid to do this one."
