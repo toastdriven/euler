1ded953a74205f0bba938002489cef8e381dc2cf
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-19 21:05:23 +0000

    problem 15 now solved with an algorithm that actually completes
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@43 735d8fd3-3d48-0410-ad09-b9318371deed

--- luke/problem-015.py
+++ luke/problem-015.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 # http://projecteuler.net/index.php?section=problems&id=15
 import math, copy
+from EulerLibs import MathLibs
 
 def get_permutations(size, hops = []):
+	"""This method will calculate the permutations correctly, but takes too long."""
 	result = 0
-	directions = (0, 1)
+	directions = ('right', 'down')
 	
 	if len(hops) == int(size * 2):
-		result += 1
+		return 1
 	else:
-		for i in directions:
-			if hops.count(i) < int(size):
-				result += get_permutations(size, hops + [i])
-			
-	return result
-	
-
-answer = get_permutations(20)
+		return sum([get_permutations(size, hops + [i]) for i in directions if hops.count(i) < int(size)])
+		
+def get_permutations_faster(size):
+	"""The fast version of get_permutations (ascertained by pattern matching to the slow one)."""
+	if size == 2:
+		return 6
+	else:
+		result = 0
+		result += get_permutations_faster(size - 1)
+		result *= (2 * size - 1)
+		result /= size
+		result *= 2
+		return result
+		
+answer = get_permutations_faster(20)
 print answer
