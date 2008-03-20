99a57a037a208c8890fc49f78a58687795cccba8
daniel <daniel@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-20 21:22:20 +0000

    Finally completed Problem 5 (the least efficient way of them all).
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@47 735d8fd3-3d48-0410-ad09-b9318371deed

--- daniel/005.py
+++ daniel/005.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python
-print "I'm too stupid to do this one."
+def factorial(num):
+    if num == 1:
+        return 1
+    
+    return num * factorial(num - 1)
+
+def check_divisible(total, current_number):
+    if current_number <= 1:
+        return True
+    
+    if total % current_number == 0:
+        return check_divisible(total, current_number - 1)
+    else:
+        return False
+
+# max_num = 10
+max_num = 20
+start_at = sum(xrange(1, max_num + 1))
+end_at = factorial(max_num)
+smallest_evenly_divisible = 0
+current = start_at
+
+# for i in xrange(start_at, end_at):
+while current <= end_at:
+    if check_divisible(current, max_num):
+        smallest_evenly_divisible = current
+        break
+    
+    current += 1
+
+print "Smallest Evenly Divisible: %s" % smallest_evenly_divisible
