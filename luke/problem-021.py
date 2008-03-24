cf5033d9c118c1cbbd00685a9b01118f7318428c
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-03-24 20:48:19 +0000

    problem 21 correct
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@49 735d8fd3-3d48-0410-ad09-b9318371deed

---
+++ luke/problem-021.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# http://projecteuler.net/index.php?section=problems&id=12
+import math
+from EulerLibs import MathLibs
+
+d = []
+for i in range(0,10001):
+	d.append(MathLibs.sumOfDivisors(i))
+	
+amicables = []
+for i in range(2,10001):
+	try:		
+		if d[d[i]] == i and d[i] != i:
+			amicables.append(i)
+	except IndexError:
+		pass
+
+answer = sum(amicables)	
+print answer
