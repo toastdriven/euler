4af5b15e99baecff6e429684e086b162585e93e6
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-04-04 15:20:15 +0000

    problem 28 correct
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@72 735d8fd3-3d48-0410-ad09-b9318371deed

--- luke/problem-028.py
+++ luke/problem-028.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # http://projecteuler.net/index.php?section=problems&id=28
+def spiral_sum(n):
+	# edge case
+	if n == 1:
+		return 1
+		
+	jump = n - 1
+	return spiral_sum(n - 2) + reduce(lambda x, y: x + y, xrange((n - 2)**2 + jump, (n - 2)**2 + (jump * 4) + 1, jump))
+	
+print spiral_sum(1001)
