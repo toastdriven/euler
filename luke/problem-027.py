b9d424b23d759c7db1a9e9e4e216cfa9a900c79a
luke <luke@735d8fd3-3d48-0410-ad09-b9318371deed>
2008-04-04 14:37:46 +0000

    problem 27 correct
    git-svn-id: svn+ssh://svn.liveoakinteractive.com/var/lib/svnroot/euler/trunk@70 735d8fd3-3d48-0410-ad09-b9318371deed

--- luke/problem-027.py
+++ luke/problem-027.py
@@ -2,3 +2,26 @@
 # http://projecteuler.net/index.php?section=problems&id=23
 from EulerLibs import MathLibs
 
+most_primes = { 'a': 1, 'b': 41, 'primes': 40 }
+
+for a in xrange(-999, 1000):
+	for b in xrange(-999, 1000):
+		n = 0
+		prime_count = 0
+		while True:
+			p = n**2 + (a * n) + b
+			if p < 2:
+				break
+			elif not MathLibs.isPrime(p):
+				break
+				
+			prime_count += 1
+			n += 1
+			
+		values = { 'a': a, 'b': b, 'primes': prime_count }
+		
+		if values['primes'] > most_primes['primes']:
+			most_primes = values
+			
+answer = most_primes['a'] * most_primes['b']
+print answer
